e92e906aa85323737fdd44820a4013457314692e
adhi85 <adimuthanikatt@gmail.com>
2024-03-20 17:51:08 +0530

    minor changes

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ app = FastAPI(
 )
 
 
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Blogs API. Visit /docs for documentation."}
+
+
 app.include_router(auth.router)
 app.include_router(adminuser.router)
 app.include_router(users.router)
